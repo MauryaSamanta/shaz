913313b7e5e43db98c939cb8c56544756db35b1f
MauryaSamanta <mauryasamanta1508@gmail.com>
2025-12-22 20:58:39 +0530

    Fixed UI issues and latest app manifest for android

--- app/android/app/src/main/java/com/shazlo/mobile/MainActivity.kt
+++ app/android/app/src/main/java/com/shazlo/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app
+package com.shazlo.mobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- app/android/app/src/main/java/com/shazlo/mobile/MainApplication.kt
+++ app/android/app/src/main/java/com/shazlo/mobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.app
+package com.shazlo.mobile
 
 import android.app.Application
 import com.facebook.react.PackageList
